a7ea2defb995f04b6da99b076f2418e2334d6593
developerNihad <nihadnahmatli@gmail.com>
2025-04-17 14:04:50 +0400

    commited

--- blog/admin.py
+++ blog/admin.py
@@ -3,10 +3,11 @@ from .models import Blog, Category
 
 
 class BlogAdmin(admin.ModelAdmin):
-    list_display=("title", "is_active", "is_home", "slug",)
-    list_editable=("is_active", "is_home",)
-    search_fields=("title", "description",)
-    readonly_fields=("slug",)
+    list_display = ("title", "is_active", "is_home", "slug",)
+    list_editable = ("is_active", "is_home",)
+    search_fields = ("title", "description",)
+    readonly_fields = ("slug",)
+    list_filter = ("is_active", "is_home")
 
 admin.site.register(Blog, BlogAdmin)
 admin.site.register(Category)
--- blog/models.py
+++ blog/models.py
@@ -3,22 +3,6 @@ from django.utils.text import slugify
 from ckeditor.fields import RichTextField
 
 
-class Blog(models.Model):
-    title = models.CharField(max_length=200)
-    image = models.ImageField(upload_to="blogs")
-    description = RichTextField()
-    is_active = models.BooleanField()
-    is_home = models.BooleanField()
-    slug = models.SlugField(null=False, blank=True, unique=True, db_index=True, editable=False)
-
-    def __str__(self):
-        return self.title
-    
-    def save(self,*args, **kwargs):
-        self.slug = slugify(self.title)
-        super().save(*args, **kwargs)
-    
-
 class Category(models.Model):
     name = models.CharField(max_length=150)
     slug = models.SlugField(null=False,blank=True, unique=True, db_index=True, editable=False)
@@ -28,4 +12,21 @@ class Category(models.Model):
     
     def save(self,*args, **kwargs):
         self.slug = slugify(self.name)
+        super().save(*args, **kwargs)
+    
+
+class Blog(models.Model):
+    title = models.CharField(max_length=200)
+    image = models.ImageField(upload_to="blogs")
+    description = RichTextField()
+    is_active = models.BooleanField()
+    is_home = models.BooleanField()
+    slug = models.SlugField(null=False, blank=True, unique=True, db_index=True, editable=False)
+    categories = models.ManyToManyField(Category)
+
+    def __str__(self):
+        return self.title
+    
+    def save(self,*args, **kwargs):
+        self.slug = slugify(self.title)
         super().save(*args, **kwargs)
